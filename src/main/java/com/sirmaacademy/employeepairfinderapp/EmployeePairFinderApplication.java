d79d3e20bd2ae00ff8b86910d50109d288344eab
Kamen <genchev.kamen@gmail.com>
2023-12-22 21:17:00 +0200

    Initial commit

---
+++ src/main/java/com/sirmaacademy/employeepairfinderapp/EmployeePairFinderApplication.java
@@ -0,0 +1,13 @@
+package com.sirmaacademy.employeepairfinderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePairFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePairFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sirmaacademy/employeepairfinderapp/EmployeePairFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirmaacademy.employeepairfinderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePairFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

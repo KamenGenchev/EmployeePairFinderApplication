e13092716b68fa6832d28dbe2528577dc8ea05a9
Kamen <genchev.kamen@gmail.com>
2023-12-24 17:33:48 +0200

    Add hibernate repository for employee work timelines

---
+++ src/main/java/com/sirmaacademy/employeepairfinderapp/repository/EmployeeProjectTimelineRepository.java
@@ -0,0 +1,7 @@
+package com.sirmaacademy.employeepairfinderapp.repository;
+
+import com.sirmaacademy.employeepairfinderapp.model.EmployeeProjectTimeline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeProjectTimelineRepository extends JpaRepository<EmployeeProjectTimeline, Long> {
+}

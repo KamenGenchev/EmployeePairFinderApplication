289f787227e945fd9f7fbb15c27f2f3620cbd6f7
Kamen <genchev.kamen@gmail.com>
2023-12-24 02:21:27 +0200

    Add functionality for creating a PostgreSQL Database if it's not present

--- src/main/java/com/sirmaacademy/employeepairfinderapp/EmployeePairFinderApplication.java
+++ src/main/java/com/sirmaacademy/employeepairfinderapp/EmployeePairFinderApplication.java
@@ -3,11 +3,41 @@ package com.sirmaacademy.employeepairfinderapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class EmployeePairFinderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeePairFinderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "password");
+            statement = connection.createStatement();
+            statement.executeQuery("SELECT count(*) FROM pg_database WHERE datname ='pairfinderdb' ");
+            ResultSet resultSet = statement.getResultSet();
+            resultSet.next();
+            int count = resultSet.getInt(1);
+
+            if (count < 0) {
+                statement.executeUpdate("CREATE DATABASE pairfinderdb");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
+
+        SpringApplication.run(EmployeePairFinderApplication.class, args);
+    }
 
 }

f6dd89bafa1a32b7970d0b451e22e769c654e273
Kamen <genchev.kamen@gmail.com>
2023-12-24 16:13:38 +0200

    Add util class for parsing different date formats

---
+++ src/main/java/com/sirmaacademy/employeepairfinderapp/util/DateFormats.java
@@ -0,0 +1,41 @@
+package com.sirmaacademy.employeepairfinderapp.util;
+
+import java.time.LocalDate;
+import java.time.format.*;
+
+public class DateFormats {
+    private static final String[] DATE_FORMATS =
+            {
+                    "dd-MM-yy",
+                    "dd-MM-yyyy",
+                    "yyyy-MM-dd",
+                    "yy-MM-dd",
+                    "dd/MM/yyyy",
+                    "dd/MM/yy",
+                    "MM/dd/yyyy",
+                    "MM/dd/yy",
+                    "yyyy/MM/dd",
+                    "yy/MM/dd",
+                    "yyyyMMdd",
+                    "yyMMdd",
+                    "dd/MM/yyyyг."
+            };
+
+    public static LocalDate convertDate(String date){
+        if (date.equals("null")) {
+            return LocalDate.now();
+        }
+
+        LocalDate localDate = null;
+        for (String dateFormat : DATE_FORMATS) {
+            try {
+                DateTimeFormatter format = DateTimeFormatter.ofPattern(dateFormat);
+                return LocalDate.parse(date, format);
+            } catch (DateTimeParseException ignore) {
+            }
+        }
+        throw new IllegalArgumentException("Date format not supported " + date);
+    }
+
+
+}

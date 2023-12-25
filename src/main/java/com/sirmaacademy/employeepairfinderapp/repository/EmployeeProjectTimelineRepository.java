fb18ad2a501f70804bc3c17bc919baadbc93c4a5
Kamen <genchev.kamen@gmail.com>
2023-12-25 04:18:19 +0200

    Add functionality to check for existing timeline

--- src/main/java/com/sirmaacademy/employeepairfinderapp/repository/EmployeeProjectTimelineRepository.java
+++ src/main/java/com/sirmaacademy/employeepairfinderapp/repository/EmployeeProjectTimelineRepository.java
@@ -1,7 +1,13 @@
 package com.sirmaacademy.employeepairfinderapp.repository;
 
+import com.sirmaacademy.employeepairfinderapp.model.Employee;
 import com.sirmaacademy.employeepairfinderapp.model.EmployeeProjectTimeline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface EmployeeProjectTimelineRepository extends JpaRepository<EmployeeProjectTimeline, Long> {
+    boolean existsByEmployeeAndProjectIdAndStartDateAndEndDate(
+            Employee employee, Long projectId, LocalDate startDate, LocalDate endDate);
 }
+

9a9d358f7fa2372e5f976c62ca5a8f3eaaee2551
Kamen <genchev.kamen@gmail.com>
2023-12-25 04:16:38 +0200

    Add repository for Employee Pairs

---
+++ src/main/java/com/sirmaacademy/employeepairfinderapp/repository/EmployeePairRepository.java
@@ -0,0 +1,8 @@
+package com.sirmaacademy.employeepairfinderapp.repository;
+
+import com.sirmaacademy.employeepairfinderapp.model.EmployeePair;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeePairRepository extends JpaRepository<EmployeePair, Long> {
+
+}
